39307e3ff2ecf47c08d5eab67d4dca8a38b56a2c
Pavel Oborin <pavel@oborin.xyz>
2020-11-29 19:25:59 +0300

    Fixes after code review

--- reducts/forms.py
+++ reducts/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class DatasetForm(forms.Form):
-    csv_string = forms.CharField(widget=forms.Textarea)
-    c_attributes = forms.CharField()
-    d_attributes = forms.CharField()
+    csv_string = forms.CharField(widget=forms.Textarea, label="Данные в формате CSV:")
+    c_attributes = forms.CharField(label="Индексы условных атрибутов через запятую:")
+    d_attributes = forms.CharField(label="Индексы целевых атрибутов через запятую:")
